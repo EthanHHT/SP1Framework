eb1890a683daa1364d0cbb0b63b46860655f4b6c
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-16 17:17:37 +0800

    Create SecurityGuard.cpp

---
+++ SecurityGuard.cpp
@@ -0,0 +1,12 @@
+  
+#include "SecurityGuard.h"
+#include "cstdlib"
+#include <time.h>
+
+int SecurityGuard::getMood()
+{
+	srand((unsigned)time(0));
+	RandMood = 1 + (rand() % 2);
+	return RandMood;
+}
+© 2021 GitHub, Inc.
