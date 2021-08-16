0671cfbe650d52a2ed50863a20a31980037cf4b7
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-16 17:16:18 +0800

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+  
+#include "Player.h"
+#include "cstdlib"
+#include <time.h>
+
+int Player::getmood()
+{
+	srand((unsigned)time(0));
+	mood = 1 + (rand() % 4);  //rng 1-4
+	return mood;
+}
+
+int Player::gethealth()
+{
+	health -= dmg_taken;
+	return health;
+}
