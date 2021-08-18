af639da7deca3342c0cd5abeed5551e2bef14584
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-18 11:37:49 +0800

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -6,3 +6,25 @@ int Enemy::getProg()
 	total_progress += progress_done;
 	return total_progress;
 }
+
+float Enemy::getmultiplier(int type_effect)
+{
+	switch (type_effect)
+	{
+	case 1:  //very effective
+		multiplier = 13 / 10;
+		break;
+	case 2:  //effective
+		multiplier = 11 / 10;
+		break; 
+	case 3:  //normal
+		multiplier = 1;
+		break;
+	case 4:  //not effective
+		multiplier = 9 / 10;
+	default:
+		break;
+	}
+
+	return multiplier;
+}
