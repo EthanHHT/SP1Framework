7ed2885ca729a8148982af7f79bf3620876a5c91
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-18 16:52:43 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,35 +7,10 @@
 using namespace std;
 
 
-void init()  //everything to init before game starts
-{
-	bool gameover = false;
-	
-	Player player;
-	player.health = 100; //idk the health i guess
-	int Mood = player.getmood();
-	switch (Mood)
-	{
-	case 1:
-		str_mood = "Agitated";
-		break;
-	case 2:
-		str_mood = "Hungover";
-		break;
-	case 3:
-		str_mood = "Elated";
-		break;
-	case 4:
-		str_mood = "Drunk";
-		break;
-	}
-	
-	cout << "Your mood is" << str_mood << endl;
-}
 
 int main(void) 
 {
-	init();
+
 	
 	movesets deck;
 
@@ -47,12 +22,7 @@ int main(void)
 	cout << "Move 1 : " << deck.getmove1() << '(' << deck.getmove1_type() << ')' << endl;
 	cout << "Move 2 : " << deck.getmove2() << '(' << deck.getmove2_type() << ')' << endl;
 	cout << "Move 3 : " << deck.getmove3() << '(' << deck.getmove3_type() << ')' << endl;
-	cout << "Move 4 : " << deck.getmove4() << '(' << deck.getmove4_type() << ')' << endl;
-	
-	while (gameover == false) //main game loop
-	{
-		
-	}		
+	cout << "Move 4 : " << deck.getmove4() << '(' << deck.getmove4_type() << ')' << endl;	
 		
 		
 
