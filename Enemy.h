87251b26f9e90c75bd6ecf89ef35d9b1a0c6459d
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-18 11:37:17 +0800

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -5,4 +5,7 @@ public:
 	int total_progress;  //% of progress completed for that enemy
 	int progress_done;  //progress completed during that turn
 	int getProg();
+	float multiplier;  //multiplier based on type effectiveness
+	int type_effect;  //type effectiveness
+	float getmultiplier(int type_effect);
 };
