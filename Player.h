79b32a60daa935d863c48174717dbb1fc4d2614a
iiaaannnn <89003887+iiaaannnn@users.noreply.github.com>
2021-08-18 09:56:06 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,9 +3,9 @@
 class Player //player class
 {
 public:
-	int mood; 
+	int mood;  //to rng the mood
 	int getmood();
 	int health;
 	int gethealth();
-	int dmg_taken;
+	int dmg_taken;  //dmg taken during that turn
 };
