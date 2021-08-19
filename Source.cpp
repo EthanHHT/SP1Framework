25560aa2fb5c314bef2f819fd88400da8edb6374
EthanHHT <88975135+EthanHHT@users.noreply.github.com>
2021-08-19 16:09:39 +0800

    characterbycharacter_output
    To output string, character by character

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,35 @@
-#include "movesets.h"
-#include "Player.h"
-#include "Enemy.h"
-#include "SecurityGuard.h"
-#include <iostream>
 #include <string>
-using namespace std;
+#include <iostream>
+#include <Windows.h>
 
-
-
-int main(void) 
+void print(std::string str)
 {
+    size_t i;
+    for (i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '.' && str[i + 1] != '.') //pause when its end of a sentence
+        {
+            std::cout << str[i]; Sleep(1000);
+        }
+        else if (str[i] == '.') //slower speed during "..."
+        {
+            std::cout << str[i]; Sleep(200);
+        }
+        else if (str[i] == ' ' && str[i] == ',') //slightly slower speed when there is a space/comma
+        {
+            std::cout << str[i]; Sleep(150);
+        }
+        else //for normal characters
+        {
+            std::cout << str[i]; Sleep(100);
+        }
+    }
+}
 
-	
-	movesets deck;
+int main(void)
+{
+    std::string str = "Hello...... world, this is a... sample output...";
+    print(str);
 
-	deck.setmove1();
-	deck.setmove2();
-	deck.setmove3();
-	deck.setmove4();
-	
-	cout << "Move 1 : " << deck.getmove1() << '(' << deck.getmove1_type() << ')' << endl;
-	cout << "Move 2 : " << deck.getmove2() << '(' << deck.getmove2_type() << ')' << endl;
-	cout << "Move 3 : " << deck.getmove3() << '(' << deck.getmove3_type() << ')' << endl;
-	cout << "Move 4 : " << deck.getmove4() << '(' << deck.getmove4_type() << ')' << endl;	
-		
-		
-
-	return 0;
-	
+    return 0;
 }
